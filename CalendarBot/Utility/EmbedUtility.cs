c6a47d9196ab6e26143cc99a808e9ffa04896fbe
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-11-08 23:55:17 +0300

    added FromEvent method to embed util

--- CalendarBot/Utility/EmbedUtility.cs
+++ CalendarBot/Utility/EmbedUtility.cs
@@ -39,7 +39,20 @@ namespace CalendarBot
             return builder.Build();
         }
 
-    public static Embed FromSuccess(string title, string description, bool addToTitle = true) =>
+        public static Embed FromEvent(CalendarEvent ev, BaseSocketClient discord) =>
+            new EmbedBuilder {
+                Title = "Scheduled Event: " + ev.Name,
+                Description = ev.Description,
+                Color = EmbedUtility.Primary,
+                Timestamp = ev.DateAndTime
+            }.AddEmptyField()
+            .AddField("Recursion Interval", ev.RecursionInterval, true)
+            .AddField("Created At", ev.CreatedAt.ToString("dd/MMM/yyyy"), true)
+            .AddField("Event Id", ev.Id)
+            .WithAuthor(discord.GetUser(ev.UserId))
+            .Build();
+
+        public static Embed FromSuccess(string title, string description, bool addToTitle = true) =>
             new EmbedBuilder {
                 Title = addToTitle ? ":white_check_mark: " : string.Empty + title,
                 Description = addToTitle ? string.Empty : ":white_check_mark: " + description,
