5d656e7ab226555b9adc8c45a3683ba62def08cc
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-11-08 23:55:02 +0300

    added AddEmptyField extension method

---
+++ CalendarBot/Extensions/EmbedBuilderExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Discord
+{
+    internal static class EmbedBuilderExtensions
+    {
+        public static EmbedBuilder AddEmptyField(this EmbedBuilder builder, bool isInline = false) =>
+            builder.AddField("\u200b", "\u200b", isInline);
+    }
+}
