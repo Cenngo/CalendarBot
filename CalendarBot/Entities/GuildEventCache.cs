ae7337650da45b90aa2c23fffb4cce0b7ea265d1
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-11-11 21:24:21 +0300

    add cache record

---
+++ CalendarBot/Entities/GuildEventCache.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CalendarBot.Entities
+{
+    public record GuildEventCache
+    {
+        public ulong GuildId { get; set; }
+        public IEnumerable<CalendarEvent> GuildEvents { get; set; }
+
+        public GuildEventCache(ulong guildId, IEnumerable<CalendarEvent> events)
+        {
+            GuildId = guildId;
+            GuildEvents = events;
+        }
+    }
+}
