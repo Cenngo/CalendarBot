2806c55e05b2e4c08a3a83e22a6437f0031b9a1e
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2021-11-11 21:21:20 +0300

    use short term cache to make autocomplete faster

--- CalendarBot/Autocompleters/EventAutocompleter.cs
+++ CalendarBot/Autocompleters/EventAutocompleter.cs
@@ -1,4 +1,6 @@
-﻿using LiteDB;
+﻿using CalendarBot.Entities;
+using LiteDB;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -12,21 +14,30 @@ namespace CalendarBot
     {
         public ILiteCollection<CalendarEvent> Events {  get; set; }
         public CultureInfo Culture {  get; set; }
+        public IMemoryCache Cache { get; set; }
 
-        public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionCommandContext context, SocketAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+        public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, SocketAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
         {
             var value = autocompleteInteraction.Data.Current.Value as string;
 
             if (string.IsNullOrEmpty(value))
                 return Task.FromResult(AutocompletionResult.FromSuccess(null));
             else {
-                var suggestions = Events.Find(x => x.Name.StartsWith(value));
+                var guildEvents = Cache.GetOrCreate(context.Guild.Id, entry => {
+                    var entryValue = new GuildEventCache(context.Guild.Id, Events.Find(x => x.GuildId == context.Guild.Id));
+                    entry.SetValue(entryValue);
+                    entry.SetSlidingExpiration(TimeSpan.FromSeconds(2));
+                    return entryValue;
+                }).GuildEvents;
+
+                var suggestions = guildEvents.Where(x => x.Name.StartsWith(value));
+
                 return Task.FromResult(AutocompletionResult.FromSuccess(suggestions
                     .Select(x => new AutocompleteResult($"{x.Name} ({x.DateAndTime.ToString(Culture.DateTimeFormat.ShortTimePattern)} {x.DateAndTime.ToString(Culture.DateTimeFormat.ShortDatePattern)})", x.Id.ToString()))));
             }
         }
 
-        protected override string GetLogString(IInteractionCommandContext context) =>
+        protected override string GetLogString(IInteractionContext context) =>
             $"Autocomplete: \"{base.ToString()}\" for {context.User} in {context.Guild}/{context.Channel}";
     }
 }
